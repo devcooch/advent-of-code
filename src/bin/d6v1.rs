804a7d306269f95eb1e877663e59db6d904468cf
Dmitry Kochkin <dmitry.kochkin@here.com>
2018-12-11 01:18:13 +0100

    Finished day 6 v1

--- src/bin/d6v1.rs
+++ src/bin/d6v1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt;
 
 #[derive(Copy, Clone, Ord, Eq, PartialOrd, PartialEq)]
@@ -39,6 +40,10 @@ fn convex_hull(points: &mut Vec<Point>) -> Vec<Point> {
     lower
 }
 
+fn distance(p1: Point, p2: Point) -> u32 {
+    ((p1.x - p2.x).abs() + (p1.y - p2.y).abs()) as u32
+}
+
 fn main() {
     let data = include_str!("day06.txt");
     let mut input: Vec<Point> = data
@@ -49,7 +54,39 @@ fn main() {
             y: item[1],
         })
         .collect();
-    for point in convex_hull(&mut input) {
-        println!("{}", point);
+    let hull = convex_hull(&mut input);
+    let left = hull.iter().map(|p| p.x).min().unwrap() as usize;
+    let right = hull.iter().map(|p| p.x).max().unwrap() as usize;
+    let top = hull.iter().map(|p| p.y).max().unwrap() as usize;
+    let bottom = hull.iter().map(|p| p.y).min().unwrap() as usize;
+    println!("{} --- {}, {} ||| {}", left, right, top, bottom);
+    println!("{} {}", input.len(), hull.len());
+    let height = top - bottom + 1;
+    let width = right - left + 1;
+    let mut grid = vec![vec![0; height]; width];
+    let mut best = HashMap::new();
+    for (i, row) in grid.iter_mut().enumerate() {
+        for (j, _) in row.iter_mut().enumerate() {
+            let candidate = Point {
+                x: (i + left) as i32,
+                y: (j + bottom) as i32,
+            };
+            let distances: Vec<_> = input.iter().map(|&p| distance(p, candidate)).collect();
+            let min_distance = distances.iter().min().unwrap();
+            if distances.iter().filter(|&d| d == min_distance).count() > 1 {
+                continue;
+            }
+            *best
+                .entry(distances.iter().position(|d| d == min_distance).unwrap())
+                .or_insert(0) += 1;
+        }
     }
+    println!(
+        "{}",
+        best.iter()
+            .filter(|&(k, _)| !hull.contains(&input[*k]))
+            .map(|(_, v)| v)
+            .max()
+            .unwrap()
+    );
 }
